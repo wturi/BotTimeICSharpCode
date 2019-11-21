0d74427f594b72e89abfb4709a05e9b4dcd5dede
luchen <luchen@encootech.onmicrosoft.com>
2019-11-21 11:06:52 +0800

    添加项目文件。

---
+++ BotTimeICSharpCode.CodeCompletion/ICSharpScriptProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace BotTimeICSharpCode.CodeCompletion
+{
+    /// <summary>
+    /// This interface allows to provide more information for scripts such as using statements, etc.
+    /// </summary>
+    public interface ICSharpScriptProvider
+    {
+        string GetUsing();
+
+        string GetVars();
+
+        string GetNamespace();
+    }
+}
