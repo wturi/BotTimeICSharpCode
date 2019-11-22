2d4cdfdffc027cfde34b7d9301d1b3ed99959dee
luchen <luchen@encootech.onmicrosoft.com>
2019-11-22 13:50:44 +0800

    补全代码

--- BotTimeICsharpCode.WpfDemo/MainWindow.xaml.cs
+++ BotTimeICsharpCode.WpfDemo/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BotTimeICSharpCode.AvalonEdit.Highlighting;
+using BotTimeICSharpCode.CodeCompletion;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BotTimeICsharpCode.WpfDemo
 {
@@ -20,9 +12,47 @@ namespace BotTimeICsharpCode.WpfDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CSharpCompletion completion;
+
+        private string _language;
+
+        public string Text { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs eventArgs)
+        {
+            base.OnInitialized(eventArgs);
+
+            _language = "C#";
+
+            completion = new CSharpCompletion(new ScriptProvider());
+            TabControl.Items.Add(OpenFile());
+        }
+
+        private TabItem OpenFile()
+        {
+            const string fileName = "csx";
+            var editor = new BotTimeCodeTextEditor
+            {
+                FontFamily = new FontFamily("Consolas"),
+                FontSize = 12,
+                Completion = completion,
+                SyntaxHighlighting = HighlightingManager.Instance.GetDefinition(_language)
+            };
+
+            editor.OpenFile(fileName, Text, _language);
+            editor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition(_language);
+
+            var tabItem = new TabItem
+            {
+                Content = editor,
+                Visibility = Visibility.Collapsed
+            };
+            return tabItem;
         }
     }
 }
---
+++ BotTimeICsharpCode.WpfDemo/ScriptProvider.cs
@@ -0,0 +1,44 @@
+﻿using BotTimeICSharpCode.CodeCompletion;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BotTimeICsharpCode.WpfDemo
+{
+    public class ScriptProvider : ICSharpScriptProvider
+    {
+        private string Vars { get; set; }
+
+        private string ImportNamespace { get; set; }
+
+        public ScriptProvider()
+        {
+            Vars = "";
+            var originalNamespaceList = new List<string>
+            {
+                "System",
+                "System.Collections.Generic",
+                "System.Linq",
+                "System.Text"
+            };
+
+            var importNamespace = originalNamespaceList.Select(d => $"using {d};");
+
+            ImportNamespace = string.Join("\r\n", importNamespace);
+        }
+
+        public string GetUsing()
+        {
+            return ImportNamespace;
+        }
+
+        public string GetVars()
+        {
+            return Vars;
+        }
+
+        public string GetNamespace()
+        {
+            return ImportNamespace;
+        }
+    }
+}
